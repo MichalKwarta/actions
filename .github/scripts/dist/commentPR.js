5aeff3a76fa21b54a3a309147e62a79df271cc95
extern-kwarta-michal <michal@cloudprizm.io>
2023-02-28 20:45:50 +0100

    asdasd

--- .github/scripts/dist/commentPR.js
+++ .github/scripts/dist/commentPR.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = async ({ github, context, core, extra }) => {
+module.exports = async ({ github, context, core }) => {
     github.rest.issues.createComment({
         issue_number: context.issue.number,
         owner: context.repo.owner,
         repo: context.repo.repo,
-        body: extra
+        body: process.env['body']
     });
 };
