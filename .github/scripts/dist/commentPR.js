989f8945835be67aeed7dabfc6345ae031c58328
extern-kwarta-michal <michal@cloudprizm.io>
2023-02-28 20:42:55 +0100

    asdasd

--- .github/scripts/dist/commentPR.js
+++ .github/scripts/dist/commentPR.js
@@ -1,9 +1,10 @@
 "use strict";
-module.exports = async ({ github, context, core }) => {
+Object.defineProperty(exports, "__esModule", { value: true });
+module.exports = async ({ github, context, core, extra }) => {
     github.rest.issues.createComment({
         issue_number: context.issue.number,
         owner: context.repo.owner,
         repo: context.repo.repo,
-        body: '👋 Hey, test job failed!'
+        body: extra
     });
 };
--- .github/scripts/ts/commentPR.ts
+++ .github/scripts/ts/commentPR.ts
@@ -7,15 +7,16 @@ import { Context } from "@actions/github/lib/context";
 type args = {
     github: ReturnType<typeof getOctokit>,
     context: Context,
-    core:any
+    core: any
+    extra: any
 }
-module.exports = async ({ github,context,core }:args) => {
+module.exports = async ({ github,context,core ,extra}:args) => {
 
     github.rest.issues.createComment({
         issue_number: context.issue.number,
         owner: context.repo.owner,
         repo: context.repo.repo,
-        body: '👋 Hey, test job failed!'
+        body: extra
     })
 
 }
